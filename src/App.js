3b79b3b99710f81280a30dfe2e8bd5f35481179d
Quandhd <quandhd94@gmail.com>
2024-11-09 09:09:24 +0700

    Cập nhật giao diện trang phim

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Browse from "./pages/browse/Browse";
 import Search from "./pages/search/Search";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Browse />} />
         <Route path="/search" element={<Search />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,52 +1,77 @@
 // src/components/MovieList.js
-import React, { useEffect, useState } from "react";
-import axios from "../axios"; // Import axios instance
-import "./MovieList.css"; // Style for movie list component
+import React, { useEffect, useState, useRef } from "react";
+import axios from "../axios";
+import "./MovieList.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-const default_image = "path/to/default/image.jpg"; // Đường dẫn đến hình ảnh mặc định
+const default_image = "path/to/default/image.jpg";
 
 function MovieList({ title, fetchUrl, isLargeRow = false, onMovieClick }) {
   const [movies, setMovies] = useState([]);
+  const [errorMovies, setErrorMovies] = useState(new Set());
+  const movieListRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(fetchUrl); // Sử dụng axios để gọi API
-      setMovies(response.data.results); // Lưu kết quả vào state
+      const response = await axios.get(fetchUrl);
+      setMovies(response.data.results);
     }
     fetchData();
   }, [fetchUrl]);
 
+  const scrollMovies = (direction) => {
+    const container = movieListRef.current;
+    const scrollAmount =
+      direction === "left" ? -container.offsetWidth : container.offsetWidth;
+    container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  };
+
+  const handleImageError = (id) => {
+    setErrorMovies((prev) => new Set(prev).add(id));
+  };
+
   return (
     <div className="movie-list">
       <h2>{title}</h2>
-      <div className="movie-list__posters">
-        {movies.map((movie) => {
-          // Kiểm tra từng trường hình ảnh
-          const imageSrc = isLargeRow
-            ? movie.poster_path
-              ? `${base_url}${movie.poster_path}`
-              : default_image // Hình mặc định nếu không có poster_path
-            : movie.backdrop_path
-              ? `${base_url}${movie.backdrop_path}`
-              : default_image; // Hình mặc định nếu không có backdrop_path
+      <div className="movie-list__container">
+        <button
+          className="scroll-button left"
+          onClick={() => scrollMovies("left")}
+        >
+          &lt;
+        </button>
+        <div className="movie-list__posters" ref={movieListRef}>
+          {movies
+            .filter((movie) => !errorMovies.has(movie.id))
+            .map((movie) => {
+              const imageSrc = isLargeRow
+                ? movie.poster_path
+                  ? `${base_url}${movie.poster_path}`
+                  : default_image
+                : movie.backdrop_path
+                ? `${base_url}${movie.backdrop_path}`
+                : default_image;
 
-          return (
-            <img
-              key={movie.id}
-              className={`movie-list__poster ${
-                isLargeRow && "movie-list__posterLarge"
-              }`}
-              src={imageSrc}
-              alt={movie.name || movie.title || "No Title Available"}
-              onClick={() => onMovieClick(movie)} // Thêm onClick handler
-              onError={(e) => {
-                e.target.onerror = null; // Ngăn việc lặp lại lỗi
-                e.target.src = default_image; // Đổi sang ảnh mặc định nếu có lỗi
-              }}
-            />
-          );
-        })}
+              return (
+                <img
+                  key={movie.id}
+                  className={`movie-list__poster ${
+                    isLargeRow && "movie-list__posterLarge"
+                  }`}
+                  src={imageSrc}
+                  alt={movie.name || movie.title || "No Title Available"}
+                  onClick={() => onMovieClick(movie)}
+                  onError={() => handleImageError(movie.id)}
+                />
+              );
+            })}
+        </div>
+        <button
+          className="scroll-button right"
+          onClick={() => scrollMovies("right")}
+        >
+          &gt;
+        </button>
       </div>
     </div>
   );
